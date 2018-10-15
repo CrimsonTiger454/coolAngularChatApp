c38333fbf7072e07011529d8ab63281c9d150033
CrimsonTiger454 <robyn@s56.net>
2018-10-15 16:00:16 -0600

    html mod

--- src/app/components/tabs/tabs.component.ts
+++ src/app/components/tabs/tabs.component.ts
@@ -32,7 +32,7 @@ export class TabsComponent implements OnInit {
   //   console.log(`${tabName} clicked`)
   // }
 
-  tabSelect(evt, cityName) {
+  tabSelect(evt, tabName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
@@ -42,7 +42,7 @@ export class TabsComponent implements OnInit {
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    document.getElementById(cityName).style.display = "block";
+    document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
 }
